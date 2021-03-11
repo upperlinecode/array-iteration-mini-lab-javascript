f6bf47dcff0c7c46ea068fd9880a88efc2753995
Aaron <aaronpfink@gmail.com>
2021-03-11 15:40:23 -0500

    fixed comments

--- iterate.js
+++ iterate.js
@@ -1,42 +1,42 @@
-# Here's an array of all of Alex's favorite foods.
+// Here's an array of all of Alex's favorite foods.
 const favorite_foods = ["Sushi", "Pizza", "Salmon", "Tamales dulces", "Yuca fries", "Apple pie", "Katsu curry", "Shrimp and grits", "Waffles"]
 
-# 1. For each food in the array, print out a statement that says "I really love _____" and then fill in the blank with each.
+// 1. For each food in the array, print out a statement that says "I really love _____" and then fill in the blank with each.
 
 
 
-# 2. Let's expand the dialogue - For each food in the array, print out three lines.
-#    "Have you ever tried ___?"
-#    "I really recommend that you try _____."
-#    "_____ is delicious."
-
-
-
-
-# 3a. Now for each food in that array, let's be specific about how MUCH Alex likes it. We'll do this without iteration first.
-# For example, print out "Alex's #1 favorite food is Sushi" and then "Alex's #2 favorite food is Pizza" and so on until you've made an ordered list.
-# To start, you can copy-paste this line as many times as you need, and make small adjustments:
-#
-# console.log("Alex's #" + (1).toString() + " favorite food is " + favorite_foods[0])
+// 2. Let's expand the dialogue - For each food in the array, print out three lines.
+//    "Have you ever tried ___?"
+//    "I really recommend that you try _____."
+//    "_____ is delicious."
 
 
 
 
-# 3b. Now comment out the above code, and try doing the same thing with a loop - you will likely need fewer lines of code.
+// 3a. Now for each food in that array, let's be specific about how MUCH Alex likes it. We'll do this without iteration first.
+// For example, print out "Alex's #1 favorite food is Sushi" and then "Alex's #2 favorite food is Pizza" and so on until you've made an ordered list.
+// To start, you can copy-paste this line as many times as you need, and make small adjustments:
+//
+// console.log("Alex's #" + (1).toString() + " favorite food is " + favorite_foods[0])
 
 
 
 
-# 4. Below is a massive array of numbers. Iterate over the array and print out only the numbers less than 100.
+// 3b. Now comment out the above code, and try doing the same thing with a loop - you will likely need fewer lines of code.
+
+
+
+
+// 4. Below is a massive array of numbers. Iterate over the array and print out only the numbers less than 100.
 const many_numbers = [109, 141, 44, 51, 133, 366, 339, 248, 226, 321, 97, 195, 245, 252, 238, 1, 366, 47, 189, 91, 148, 88, 194, 106, 5, 128, 165, 337, 380, 181, 143, 95]
 
 
 
-# 5. Now iterate over the array but print out only the even ones.
+// 5. Now iterate over the array but print out only the even ones.
 
 
 
-# 6. Below is an array of 99 names. Iterate over them all and print out only the names that include the letter "a".
+// 6. Below is an array of 99 names. Iterate over them all and print out only the names that include the letter "a".
 const many_names = ["Alexa","Burke","Kasimir","Baxter","Carissa","Vielka","Derek","Jemima","Jackson","Keegan","Graham","Melissa","Jeanette","Grant","Kirsten","Naida","Brody","Ishmael","Kane","Seth","Rae","Eagan","Camille","Alana",
   "Vance","Melinda","Tarik","Risa","Jordan","Camilla","Karly","Baker","Adena","Calvin","Kendall","Nasim","Kellie","Dana","Rhoda","Linus","Tyler","Ahmed","Dante","Shay","Lael","Tana","Claudia","Chadwick","Tara",
   "Fulton","Justine","Malcolm","Rowan","Christopher","Ciaran","Ivan","Hiram","Blake","Colton","Nathaniel","Moses","Cynthia","George","Ignacia","Chanda","Wyatt","Amethyst","Vladimir","Adam","Boris","Joseph","Scarlett","Kieran","Curran",
@@ -44,20 +44,20 @@ const many_names = ["Alexa","Burke","Kasimir","Baxter","Carissa","Vielka","Derek
 
 
 
-# 7. Now iterate over the array and print out only the names which have more than 6 letters in them.
+// 7. Now iterate over the array and print out only the names which have more than 6 letters in them.
 
 
 
-# 8. Iterate over the array and find the longest name in the whole thing. Then print out "The longest name in the array is ______. It has ___ letters in it."
+// 8. Iterate over the array and find the longest name in the whole thing. Then print out "The longest name in the array is ______. It has ___ letters in it."
 
 
 
-# 9. Take that same array and sort it into two arrays: one array of long names, which have more than 6 characters, and one array of short names which have 6 or fewer characters. Then print out both arrays.
+// 9. Take that same array and sort it into two arrays: one array of long names, which have more than 6 characters, and one array of short names which have 6 or fewer characters. Then print out both arrays.
 
 
 
-# 10. Are there more long names or short names? Write code to figure that out.
+// 10. Are there more long names or short names? Write code to figure that out.
 
 
 
-# CHALLENGE: Iterate over the array of numbers to find the average of them all.
+// CHALLENGE: Iterate over the array of numbers to find the average of them all.
